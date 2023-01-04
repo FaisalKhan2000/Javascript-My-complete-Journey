63fd3f9b6694b0f2d2fb5049e644e6a1b4630029
Faisal Khan <iamfaisal.luv@gmail.com>
2023-01-05 01:36:24 +0530

    Modifying ObjectPrimitive

--- 2_objectsBasics/7_objectToPrimitiveConversion.js
+++ 2_objectsBasics/7_objectToPrimitiveConversion.js
@@ -21,21 +21,21 @@
 // console.log(user + 500); // hint: default -> 1500
 
 //* toString/valueOf
-let user = {
-  name: "John",
-  money: 1000,
+// let user = {
+//   name: "John",
+//   money: 1000,
 
-  // for hint="string"
-  toString() {
-    return `{name: "${this.name}"}`;
-  },
+//   // for hint="string"
+//   toString() {
+//     return `{name: "${this.name}"}`;
+//   },
 
-  // for hint="number" or "default"
-  valueOf() {
-    return this.money;
-  },
-};
+//   // for hint="number" or "default"
+//   valueOf() {
+//     return this.money;
+//   },
+// };
 
-console.log(`${user}`); // toString -> {name: "John"}
-console.log(+user); // valueOf -> 1000
-console.log(user + 500); // valueOf -> 1500
+// console.log(`${user}`); // toString -> {name: "John"}
+// console.log(+user); // valueOf -> 1000
+// console.log(user + 500); // valueOf -> 1500
