f4813e267775bafb4a51d45c990ab8a74498f1d9
Faisal Khan <iamfaisal.luv@gmail.com>
2023-01-07 12:47:12 +0530

    changes in 11_dataAndTime.js

--- 3_dataTypes/12_jsonMethods.js
+++ 3_dataTypes/12_jsonMethods.js
@@ -425,3 +425,21 @@ let meetup5 = JSON.parse(str, function (key, value) {
 });
 
 console.log(meetup5.date.getDate());
+
+// By the way, that works for nested objects as well:
+
+/*
+
+* Summary
+
+    * JSON is a data format that has its own independent standard and libraries for most programming languages.
+
+    * JSON supports plain objects, arrays, strings, numbers, booleans, and null.
+
+    * JavaScript provides methods JSON.stringify to serialize into JSON and JSON.parse to read from JSON.
+
+    * Both methods support transformer functions for smart reading/writing.
+
+    * If an object has toJSON, then it is called by JSON.stringify.
+
+*/
