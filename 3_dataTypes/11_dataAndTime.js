06681967a58718b46912f126077a7fdc753adf7c
Faisal Khan <iamfaisal.luv@gmail.com>
2023-01-07 12:48:23 +0530

    Adding 11_dataAndTime.js /12_jsonMethods.js in 3_dataTypes module

--- 3_dataTypes/11_dataAndTime.js
+++ 3_dataTypes/11_dataAndTime.js
@@ -261,13 +261,20 @@ console.log(date);
 
 /*
 
-Date and time in JavaScript are represented with the Date object. We can’t create “only date” or “only time”: Date objects always carry both.
-Months are counted from zero (yes, January is a zero month).
-Days of week in getDay() are also counted from zero (that’s Sunday).
-Date auto-corrects itself when out-of-range components are set. Good for adding/subtracting days/months/hours.
-Dates can be subtracted, giving their difference in milliseconds. That’s because a Date becomes the timestamp when converted to a number.
-Use Date.now() to get the current timestamp fast.
+* Date and time in JavaScript are represented with the Date object. We can’t create “only date” or “only time”: Date objects always carry both.
+
+* Months are counted from zero (yes, January is a zero month).
+
+* Days of week in getDay() are also counted from zero (that’s Sunday).
+
+* Date auto-corrects itself when out-of-range components are set. Good for adding/subtracting days/months/hours.
+
+* Dates can be subtracted, giving their difference in milliseconds. That’s because a Date becomes the timestamp when converted to a number.
+
+* Use Date.now() to get the current timestamp fast.
+
 Note that unlike many other systems, timestamps in JavaScript are in milliseconds, not in seconds.
+
 
 Sometimes we need more precise time measurements. JavaScript itself does not have a way to measure time in microseconds (1 millionth of a second), but most environments provide it. For instance, browser has performance.now() that gives the number of milliseconds from the start of page loading with microsecond precision (3 digits after the point):
 
